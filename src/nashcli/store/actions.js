dffad2c1013e4d9037e2b8e87917e04463fbd6c8
Saber <saber@localhost.localdomain>
2018-12-11 09:30:55 +0000

    Pause on UI, go fix issue in backend for Satoshi Typeforce Check

--- src/nashcli/store/actions.js
+++ src/nashcli/store/actions.js
@@ -1,8 +1,6 @@
-import * as types from './mutation-types';
-import { query } from '../export';
 import co from 'co';
-import Bluebird from 'bluebird';
-import { signTX } from '../export';
+import * as types from './mutation-types';
+import { query, signTX } from '../export';
 
 export const updateCreds = ({ commit }, val) => {
   commit(types.setCreds, val);
@@ -14,14 +12,11 @@ export const updateRootTXFilter = ({ commit }, val) => {
 
 export const updateRootAccountBalance = ({ commit, state }, payload) => {
   co(query('accounts', 'balance', payload, state.creds)).then((res) => {
-    console.log('Returned Res');
-    console.log(res);
     commit(types.setRootAccountBalance, res.res.body);
   });
 };
 
 export const updateRootAccountTransactions = ({ commit, state }, payload) => {
-  console.log('RootAccount TX called...'); 
   co(function* () {
     // TODO: Remove Hardcode
     const paymentList = yield query('payments', 'list', payload, state.creds);
@@ -29,15 +24,15 @@ export const updateRootAccountTransactions = ({ commit, state }, payload) => {
     // Label the paymentList and transferList
     const deposits = paymentList.res.body.reduce((acc, row) => {
       // const newRow = Object.assign(row, { category: 'deposit' });
-      const received = row.txs.reduce((aacc, row) => {
-        const recv = parseFloat(parseFloat(row.received) / 100000000.0);
+      const received = row.txs.reduce((aacc, roww) => {
+        const recv = parseFloat(parseFloat(roww.received) / 100000000.0);
         return aacc + recv;
       }, 0);
-      const confirmations = row.txs.reduce((aacc, row, i) => {
-        const txCfm = row.confirmations;
-        if (txCfm < aacc && aacc !== -1) { return txCfm };
+      const confirmations = row.txs.reduce((aacc, roww, i) => {
+        const txCfm = roww.confirmations;
+        if (txCfm < aacc && aacc !== -1) { return txCfm; }
         if (aacc === -1) return txCfm;
-        return aacc; 
+        return aacc;
       }, -1);
       const newRow = {
         id: row.id,
@@ -67,9 +62,7 @@ export const updateRootAccountTransactions = ({ commit, state }, payload) => {
       return acc.concat(newRow);
     }, []);
     const txList = Array.concat(deposits, transfers);
-    console.log(txList);
     commit(types.setRootAccountTransactions, txList); // TODO Remove HardCode
-
   }).catch((err) => {
     if (err) {
       console.log(err);
@@ -79,15 +72,15 @@ export const updateRootAccountTransactions = ({ commit, state }, payload) => {
 
 export const sendTransferRequest = ({ commit, state }, payload) => {
   co(function* () {
-    const sendTransfer = { addresses: payload.addresses, slave: 'default' };
-    const { store: transferStore, types: transferTypes } = payload;
     // TODO: Remove Hardcode
+    const sendTransfer = { addresses: payload.addresses, slave: 'default' };
+    console.log(`sendTransferRequest : ${sendTransfer}`);
+    const { store: transferStore, types: transferTypes } = payload;
     const transferResp = yield query('transfers', 'makeRequest', sendTransfer, state.creds);
     console.log('transferResp');
     console.log(JSON.stringify(transferResp, null, 2));
     transferStore.dispatch('updateTransferResponse', transferResp.res.body);
     transferStore.dispatch('updateTransferStep', transferTypes.stepEnum.REVIEW);
-
   }).catch((err) => {
     if (err) {
       console.log(err);
